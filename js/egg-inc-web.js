5bd8e58dae427036e1a4dd5e721bc09d8def2ed2
Arik Yavilevich <arik@yavilevich.com>
2021-03-01 00:46:52 +0200

    add web wrapper for existing query types

--- js/egg-inc-web.js
+++ js/egg-inc-web.js
@@ -1,5 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const createError = require('http-errors');
+
+require('dotenv').config();
+
 const EggIncApi = require('./egg-inc-api');
 
 // web server around the API
+// setup express
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// TODO
+// define end points
+app.get('/getAllActiveContracts', (req, res) => {
+    EggIncApi.getPeriodicals().then((data) => {
+        res.send(data.periodicals.contracts);
+    });
+});
+
+app.get('/events', (req, res) => {
+    EggIncApi.getPeriodicals().then((data) => {
+        res.send(data.periodicals.events);
+    });
+});
+
+app.get('/getCoopStatus', (req, res) => {
+    EggIncApi.getCoopStatus(req.query.contract, req.query.coop).then((data) => {
+        res.send(data.status);
+    });
+});
+
+app.get('/getPlayerInfo', (req, res) => {
+    EggIncApi.getPlayerInfo(req.query.playerId).then((data) => {
+        res.send(data.payload.data);
+    });
+});
+
+app.get('/notFound', (/*req, res, next*/) => {
+    throw new createError.NotFound();
+});
+
+(async function () {
+    console.log('Starting!');
+
+    // start web server
+    app.listen(process.env.EI_API_PORT, () => {
+        console.log(`App started, port: ${process.env.EI_API_PORT}, running as: ${process.env.NODE_ENV}`);
+    });
+})()
+
+module.exports = app;
+
+/*
+Usage examples:
+npm run start-ei-web
+http://localhost:6001/getAllActiveContracts
+http://localhost:6001/events
+http://localhost:6001/getCoopStatus?contract=new-moon&coop=dmv
+http://localhost:6001/getPlayerInfo?playerId=EI6411720689451008
+*/
