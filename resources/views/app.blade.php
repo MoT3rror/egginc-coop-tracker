805a4fd7672b9561cc8c0965049c51fc608e5fe2
Joshua Bixler <joshbixler@sbcglobal.net>
2021-09-25 20:45:51 -0500

    Add goat counter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <script data-goatcounter="https://mot3rror.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>
         <script src="{{ mix('/js/app.js') }}" defer></script>
         <title>MoT3rror Egg Inc PlayGround</title>
         <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
