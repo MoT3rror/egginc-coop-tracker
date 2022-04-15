986ed483d25361c7680542fe9abc84c136a4e849
Joshua Bixler <joshbixler@sbcglobal.net>
2022-04-14 21:32:56 -0500

    Delete complete coops channels as slash

--- app/DiscordMessages/DeleteCompleteChannels.php
+++ app/DiscordMessages/DeleteCompleteChannels.php
@@ -5,7 +5,7 @@ class DeleteCompleteChannels extends Status
 {
     protected $middlewares = ['requiresGuild', 'isAdmin'];
 
-    public $guildOnly = false;
+    public $guildOnly = true;
 
     public function message(): array
     {
@@ -29,5 +29,25 @@ class DeleteCompleteChannels extends Status
     public function help(): string
     {
         return '{Contract ID} - Delete coop channels in mass that are complete.';
+    }
+
+    public function description(): string
+    {
+        return 'Delete coop channels in mass that are complete.';
+    }
+
+    public function options(): array
+    {
+        $contracts = $this->getAvailableContractOptions();
+
+        return [
+            [
+                'type'        => 3,
+                'name'        => 'contract_id',
+                'description' => 'Contract ID',
+                'required'    => true,
+                'choices'     => $contracts,
+            ],
+        ];
     }
 }
