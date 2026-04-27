93e22bf00a41ed749ab365534c40c4caa48a6344
MoT3rror <joshbixler047@gmail.com>
2026-04-27 17:08:47 +0000

    Update client and app version to 71 and 1.35.5 respectively

--- js/egg-inc-api.js
+++ js/egg-inc-api.js
@@ -6,8 +6,8 @@ const root = protobuf.loadSync('js/Proto/egginc.proto');
 
 // default values and consts
 const EI_USER_ID = 'EI6291940968235008';
-const CLIENT_VERSION = 70;
-const APP_VERSION = '1.35';
+const CLIENT_VERSION = 71;
+const APP_VERSION = '1.35.5';
 
 const ei_request = (path, payload, requestPB, responsePB) => {
     return new Promise((resolve, reject) => {
